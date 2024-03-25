0e1b8a1928cc35e5af6f3cca937c44d86d6f6028
Christian Tragesser <christian@evoen.net>
2024-03-25 09:15:15 -0500

    VScode extensions

--- init.sh
+++ init.sh
@@ -14,22 +14,33 @@ function announce_platform() {
     printf "\nFound platform ${instance_platform} ${instance_version}.\n"
 }
 
+function install_vscode_extensions() {
+    code --install-extension vscodevim.vim
+    code --install-extension shardulm94.trailing-spaces
+    code --install-extension bierner.markdown-preview-github-styles
+    code --install-extension golang.go
+    code --install-extension ms-python.python
+    code --install-extension 4ops.terraform
+}
+
 if [[ "${PLATFORM}" == 'Linux' ]]; then
     if [ -f /etc/redhat-release ]; then
         distro=$(cat /etc/redhat-release | awk '{print $1}')
         announce_platform ${PLATFORM} ${distro}
 
         if [[ "${distro}" == 'Fedora' ]]; then
-            # Add Google Chrome repository
-            sudo dnf config-manager --add-repo https://dl.google.com/linux/chrome/rpm/stable/x86_64
-
             # Add VSCode repository
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
             sudo dnf update
 
-            sudo dnf install -y code curl git google-chrome-stable golang gnome-tweaks helm htop jq podman vim
+            sudo dnf install -y code curl fedora-workstation-repositories flatpak git golang gnome-tweaks helm htop jq podman vim
+
+            sudo flatpak remote-add --if-not-exists flathub
+            sudo flatpak install flathub com.google.Chrome -y
+
+            ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''
 
             # Install golangci-lint
             if ! $(go env GOPATH)/bin/golangci-lint version &>/dev/null; then
@@ -45,10 +56,16 @@ if [[ "${PLATFORM}" == 'Linux' ]]; then
                 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v${KIND_VERSION}/kind-linux-amd64
                 chmod +x ./kind
                 sudo mv ./kind /usr/local/bin/kind
+                kind version
             else
                 printf "\nFound KIND.\n"
                 kind version
             fi
+
+            install_vscode_extensions
+            cp ./config/Code/User/keybindings.json ~/.config/Code/User/
+
+            gsettings set org.gnome.desktop.sound event-sounds false
 
             cp -R ./.* ~/
         fi
@@ -73,7 +90,9 @@ elif [[ "${PLATFORM}" == 'Darwin' ]]; then
     brew install --cask iterm2 slack visual-studio-code
 
     brew install colima
+
+    install_vscode_extensions
+    cp ./config/Code/User/keybindings.json ~/Library/Application Support/Code/User/
 else
     printf "\nThis script does not support ${PLATFORM} systems.\n"
 fi
-
